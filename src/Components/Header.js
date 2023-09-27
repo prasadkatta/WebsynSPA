a3274b488b55d47f5f2a1a35cd8163283a86f7ab
Krishna Prasad Katta <krishnaprasad.katta@websynergies.com>
2023-09-27 12:54:39 +0530

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,9 +16,9 @@ function Header() {
             <div className='container'>
                 <nav className="navbar navbar-expand-lg">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">
+                        <Link className="navbar-brand" to="/">
                             <img src={logo} alt="log" />
-                        </a>
+                        </Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>

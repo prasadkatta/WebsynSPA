18bd517fcb3fbbef40e22d45ce75d5e599c75479
Krishna Prasad Katta <krishnaprasad.katta@websynergies.com>
2023-09-27 14:26:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Header from './Components/Header';
 import Home from './Components/Home';
